2f857acb310e13ab8e6017e4350bcaed8befa6eb
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-26 13:29:53 +0700

    feat(exercise1) done exercise 1

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,18 +1,33 @@
 package com.adepuu.exercises.session6;
 
-public class Exercise1 {
-    /**
-     * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
-     *
-     * Input: 1, 2, 3, 4, 5, q
-     * Output: 3
-     *
-     * Input: 1, 2, 3, a, b, 4, 5, q
-     * Output: 3
-     *
-     * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
-     */
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+
+        while (true){
+            System.out.print("Enter numbers to calculate average, enter 'q' to finish : ");
+            String input = scanner.next();
+            if (input.equalsIgnoreCase("q")){
+                break;
+            }
+            try {
+                int num = Integer.parseInt(input);
+                sum = sum + num;
+                count++;
+            } catch (NumberFormatException e){
+                System.out.println("Invalid input, please enter a valid number or enter 'q' to finish");
+            }
+
+        }
+            if (count > 0){
+                double average = (double) sum / count;
+                System.out.println("Average : " + average);
+            } else {
+                System.out.println("No valid numbers entered");
+            }
     }
-}
+    }
