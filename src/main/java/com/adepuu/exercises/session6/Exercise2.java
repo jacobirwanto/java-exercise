9096aa30d31ff9f496a2aadc784c580cafd11ab8
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-26 14:12:34 +0700

    feat(exercise2) done exercise 2

--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -2,17 +2,25 @@ package com.adepuu.exercises.session6;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Exercise2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter the file name: ");
         String fileName = scanner.nextLine();
-        readFileContents(fileName);
-        scanner.close();
+        try {
+            readFileContents(fileName);
+        } catch (IOException e){
+            System.out.println("Error reading the file : " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 
-    static void readFileContents(String fileName) {
+    static void readFileContents(String fileName) throws IOException {
         BufferedReader reader = null;
         reader = new BufferedReader(new FileReader(fileName));
         String line;
